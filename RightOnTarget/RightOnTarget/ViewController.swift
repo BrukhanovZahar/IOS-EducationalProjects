0d45b5c95a9d971d6c72a8f1acb6d44c6e1d617c
ZDBryukhanov <zdbryukhanov@mai.education>
2024-02-15 03:32:10 +0300

    add and configure button and slider

--- RightOnTarget/RightOnTarget/ViewController.swift
+++ RightOnTarget/RightOnTarget/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     
     let slider = UISlider()
+    let button = UIButton()
+    let label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,17 +21,53 @@ class ViewController: UIViewController {
     
     func setupLayout() {
         view.addSubview(slider)
+        view.addSubview(button)
+        view.addSubview(label)
         
+        configureView()
         configureSlider()
+        configureButton()
+        configureLabel()
+    }
+    
+    func configureView() {
+        view.backgroundColor = .systemYellow
     }
     
     func configureSlider() {
         slider.translatesAutoresizingMaskIntoConstraints = false
-        
+        slider.minimumTrackTintColor = .systemPink
+        slider.thumbTintColor = .systemPink
+        slider.tintColor = .systemPurple
+        slider.minimumValueImage = UIImage(systemName: "01.square.fill")
+        slider.maximumValueImage = UIImage(systemName: "50.square.fill")
+                
         NSLayoutConstraint.activate([
             slider.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
             slider.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             slider.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 20)
+        ])
+    }
+    
+    func configureButton() {
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Проверить", for: .normal)
+        button.setTitleColor(.systemIndigo, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            button.topAnchor.constraint(equalTo: slider.bottomAnchor, constant: 30)
+        ])
+    }
+    
+    func configureLabel() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "0"
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            label.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 30)
         ])
     }
 
