60633bd59be8bbc93d2fdfbef58c4eccf0398555
ZDBryukhanov <zdbryukhanov@mai.education>
2024-02-18 02:15:28 +0300

    added lifecycle methods

--- RightOnTarget/RightOnTarget/ViewController.swift
+++ RightOnTarget/RightOnTarget/ViewController.swift
@@ -33,6 +33,26 @@ class ViewController: UIViewController {
         label.text = "\(number)"
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
+    
     func setupLayout() {
         view.addSubview(slider)
         view.addSubview(button)
