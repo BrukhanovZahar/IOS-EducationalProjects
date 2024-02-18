c8d7cbd36993655c97822a5065bb8a2b8b7141b0
ZDBryukhanov <zdbryukhanov@mai.education>
2024-02-19 01:58:42 +0300

    add game class

--- RightOnTarget/RightOnTarget/Model/Game.swift
+++ RightOnTarget/RightOnTarget/Model/Game.swift
@@ -17,3 +17,55 @@ protocol GameProtocol {
     func calculateSCORE(with value: Int)
 }
 
+
+class Game: GameProtocol {
+    var score: Int = 0
+    private var minSecretValue: Int
+    private var maxSecretValue: Int
+    var currentSecretValue: Int = 0
+    private var lastRound: Int
+    private var currentRound: Int = 1
+    var isGameEnded: Bool {
+        if currentRound >= lastRound {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    init?(startValue: Int, endValue: Int, rounds: Int) {
+        guard startValue <= endValue else {
+            return nil
+        }
+        
+        minSecretValue = startValue
+        maxSecretValue = endValue
+        lastRound = rounds
+        currentSecretValue = getNewSecretValue()
+    }
+    
+    func restartGame() {
+        currentRound = 0
+        score = 0
+        startNewRound()
+    }
+    
+    func startNewRound() {
+        currentSecretValue = getNewSecretValue()
+        currentRound += 1
+    }
+    
+    private func getNewSecretValue() -> Int {
+        return (minSecretValue...maxSecretValue).randomElement() ?? 0
+    }
+    
+    func calculateSCORE(with value: Int) {
+        if value > currentSecretValue {
+            score += 50 - value + currentSecretValue
+        } else if value < currentSecretValue {
+            score += 50 - currentSecretValue + value
+        } else {
+            score += 50
+        }
+    }
+}
