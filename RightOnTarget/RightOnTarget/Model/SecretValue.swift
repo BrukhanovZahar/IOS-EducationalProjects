c7367689334cc42dd08f2145f3c957088e072fbe
ZDBryukhanov <zdbryukhanov@mai.education>
2024-02-26 00:41:34 +0300

    add secret value type

--- RightOnTarget/RightOnTarget/Model/SecretValue.swift
+++ RightOnTarget/RightOnTarget/Model/SecretValue.swift
@@ -5,4 +5,27 @@
 //  Created by Захар Брюханов on 25.02.2024.
 //
 
-import Foundation
+typealias SecretNumericValue = SecretValue<Int>
+typealias SecretColorValue = SecretValue<Color>
+
+protocol SecretValueProtocol {
+    associatedtype ValueType
+    var value: ValueType { get }
+    
+    mutating func setRandomValue()
+}
+
+struct SecretValue<T: Equatable>: SecretValueProtocol {
+    typealias ValueType = T
+    var value: T
+    
+    private let randomValueClosure: (T) -> T
+    init(initialValue: T, randomValueClosure: @escaping (T) -> T) {
+        value = initialValue
+        self.randomValueClosure = randomValueClosure
+    }
+    
+    mutating func setRandomValue(){
+        value = randomValueClosure(value)
+    }
+}
