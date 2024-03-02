ea4d2b3c65f23b60359db5062bb43f8e2840750a
ZDBryukhanov <zdbryukhanov@mai.education>
2024-03-03 01:32:08 +0300

    add contacts at the table

--- ContactList/ContactList/ViewController.swift
+++ ContactList/ContactList/ViewController.swift
@@ -8,18 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var contacts: [ContactProtocol] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loadContacts()
     }
 
+    private func loadContacts() {
+        contacts.append(Contact(title: "Саня Техосмотр", phone: "+799912312323"))
+        contacts.append(Contact(title: "Владимир Анатольевич", phone: "+781213342321"))
+        contacts.append(Contact(title: "Сильвестр", phone: "+7000911112"))
+        contacts.sort{ $0.title < $1.title }
+    }
 
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return contacts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -43,7 +52,8 @@ extension ViewController: UITableViewDataSource {
     
     private func configure(cell: inout UITableViewCell, for indexPath: IndexPath) {
         var configuration = cell.defaultContentConfiguration()
-        configuration.text = "Строка \(indexPath.row)"
+        configuration.text = contacts[indexPath.row].title
+        configuration.secondaryText = contacts[indexPath.row].phone
         cell.contentConfiguration = configuration
     }
     
